135402673d849e86d45e77ac70a577a1973a77b0
rahnoff <72708227+rahnoff@users.noreply.github.com>
2025-05-27 17:37:31 +0300

    Update V1__Initial_schema.sql

--- postgresql/flyway/V1__Initial_schema.sql
+++ postgresql/flyway/V1__Initial_schema.sql
@@ -5,7 +5,7 @@ CREATE SCHEMA IF NOT EXISTS enrollments;
 CREATE TABLE IF NOT EXISTS enrollments.employees
 (
     id         uuid        PRIMARY KEY,
-    create_at  timestamptz NOT NULL,
+    created_at timestamptz NOT NULL,
     edited_at  timestamptz NOT NULL,
     first_name text        NOT NULL,
     last_name  text        NOT NULL
@@ -13,10 +13,10 @@ CREATE TABLE IF NOT EXISTS enrollments.employees
 
 CREATE TABLE IF NOT EXISTS enrollments.courses
 (
-    id        uuid        PRIMARY KEY,
-    create_at timestamptz NOT NULL,
-    edited_at timestamptz NOT NULL,
-    title     text        NOT NULL
+    id         uuid        PRIMARY KEY,
+    created_at timestamptz NOT NULL,
+    edited_at  timestamptz NOT NULL,
+    title      text        NOT NULL
 );
 
 CREATE TYPE statuses AS ENUM
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS enrollments.enrollments
 (
     id          uuid        PRIMARY KEY,
     course_id   uuid        NOT NULL REFERENCES enrollments.courses(id),
-    create_at   timestamptz NOT NULL,
+    created_at  timestamptz NOT NULL,
     edited_at   timestamptz NOT NULL,
     employee_id uuid        NOT NULL REFERENCES enrollments.employees(id),
     status      statuses    NOT NULL

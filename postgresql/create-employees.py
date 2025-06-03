b7be30b3f5410eb039e0b36bfc516211c2243c93
rahnoff <72708227+rahnoff@users.noreply.github.com>
2025-06-03 15:20:22 +0300

    Update create-employees.py

--- postgresql/create-employees.py
+++ postgresql/create-employees.py
@@ -3,11 +3,20 @@ import csv
 import faker
 
 
-faker: faker.proxy.Faker = faker.Faker()
-first_names: list[str] = [faker.first_name() for first_name in range(10000)]
-last_names: list[str] = [faker.last_name() for last_name in range(10000)]
-ids: list[str] = [faker.uuid4() for id in range(10000)]
-with open('employees.csv', 'w', newline='') as employees_csv:
-    writer: _csv.writer = csv.writer(employees_csv, dialect='unix', delimiter=',', escapechar='\\', quoting=csv.QUOTE_NONE)
-    for record in zip(ids, first_names, last_names):
-        writer.writerow(record)
+def create_employees_csv() -> None:
+    faker: faker.proxy.Faker = faker.Faker()
+    first_names: list[str] = [faker.first_name() for first_name in range(10000)]
+    last_names: list[str] = [faker.last_name() for last_name in range(10000)]
+    ids: list[str] = [faker.uuid4() for id in range(10000)]
+    with open('employees.csv', 'w', newline='') as employees_csv:
+        writer: _csv.writer = csv.writer(employees_csv, dialect='unix', delimiter=',', escapechar='\\', quoting=csv.QUOTE_NONE)
+        for record in zip(ids, first_names, last_names):
+            writer.writerow(record)
+
+
+def main() -> None:
+    create_employees_csv()
+
+
+if __name__ == '__main__':
+    main()

5cea68a9128d4301d877c1a101ad2e555f4d3814
rahnoff <72708227+rahnoff@users.noreply.github.com>
2025-06-03 15:17:57 +0300

    Update create-employees.py

--- postgresql/create-employees.py
+++ postgresql/create-employees.py
@@ -7,3 +7,7 @@ faker: faker.proxy.Faker = faker.Faker()
 first_names: list[str] = [faker.first_name() for first_name in range(10000)]
 last_names: list[str] = [faker.last_name() for last_name in range(10000)]
 ids: list[str] = [faker.uuid4() for id in range(10000)]
+with open('employees.csv', 'w', newline='') as employees_csv:
+    writer: _csv.writer = csv.writer(employees_csv, dialect='unix', delimiter=',', escapechar='\\', quoting=csv.QUOTE_NONE)
+    for record in zip(ids, first_names, last_names):
+        writer.writerow(record)

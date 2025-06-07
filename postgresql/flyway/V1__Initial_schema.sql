4d71327317171638d2b592da5b27e3ffd23f1422
rahnoff <72708227+rahnoff@users.noreply.github.com>
2025-06-07 21:21:19 +0300

    Update V1__Initial_schema.sql

--- postgresql/flyway/V1__Initial_schema.sql
+++ postgresql/flyway/V1__Initial_schema.sql
@@ -1,6 +1,7 @@
 REVOKE CREATE ON SCHEMA public FROM PUBLIC;
 
 
+-- Application schema
 CREATE SCHEMA IF NOT EXISTS enrollments;
 
 
@@ -57,6 +58,7 @@ CREATE TABLE IF NOT EXISTS enrollments.enrollments
 );
 
 
+-- Auxiliary stuff
 CREATE OR REPLACE FUNCTION update_edited_at() RETURNS TRIGGER AS
 $$
     BEGIN
@@ -90,6 +92,3 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS enrollments.enrollments_m_view AS
             INNER JOIN enrollments.employees AS em ON en.employee_id = em.id
             INNER JOIN enrollments.courses AS co ON en.course_id = co.id
 WITH DATA;
-
-
-INSERT INTO enrollments.courses (id, created_at, edited_at, title) VALUES ('3b854085-d26a-4f8c-90f5-36abbf1756c0', )

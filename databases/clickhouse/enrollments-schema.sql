0d11899af648f91310587152758137cfb5cae5e7
rahnoff <72708227+rahnoff@users.noreply.github.com>
2026-03-09 20:16:17 +0300

    Update enrollments-schema.sql

--- databases/clickhouse/enrollments-schema.sql
+++ databases/clickhouse/enrollments-schema.sql
@@ -3,11 +3,11 @@ CREATE DATABASE IF NOT EXISTS dream_stream ON CLUSTER default ENGINE = Atomic;
 
 CREATE TABLE IF NOT EXISTS dream_stream.enrollments_local ON CLUSTER default
 (
-    completed_at         DateTime64(9) CODEC(DoubleDelta, ZSTD(1)),
+    completed_at         DateTime64(3) CODEC(DoubleDelta, ZSTD(1)),
     course_name          LowCardinality(String),
     course_category      LowCardinality(String),
     employee_name        LowCardinality(String),
-    enrolled_at          DateTime64(9) CODEC(DoubleDelta, ZSTD(1)),
+    enrolled_at          DateTime64(3) CODEC(DoubleDelta, ZSTD(1)),
     status               Enum8('Cancelled' = 0, 'Completed' = 1, 'Enrolled' = 2, 'In progress' = 3)
 )
 ENGINE = ReplicatedMergeTree('/clickhouse/tables/{shard}/{database}/{table}', '{replica}')

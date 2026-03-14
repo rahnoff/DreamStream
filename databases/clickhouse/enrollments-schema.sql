3399208fcf1efecad62616f55935cc46858931e6
rahnoff <72708227+rahnoff@users.noreply.github.com>
2026-03-14 21:00:09 +0300

    Update enrollments-schema.sql

--- databases/clickhouse/enrollments-schema.sql
+++ databases/clickhouse/enrollments-schema.sql
@@ -5,13 +5,17 @@ CREATE TABLE IF NOT EXISTS dream_stream.enrollments_local ON CLUSTER default
 (
     completed_at         DateTime64(3) CODEC(DoubleDelta, ZSTD(1)),
     course_name          LowCardinality(String),
-    course_category      LowCardinality(String),
+    course_category      Enum8('Finance' = 0, 'HR' = 1, 'IT' = 2, 'Marketing' = 3),
+    edited_at            DateTime64(3) CODEC(DoubleDelta, ZSTD(1)),
+    employee_id          UUID,
     employee_name        LowCardinality(String),
     enrolled_at          DateTime64(3) CODEC(DoubleDelta, ZSTD(1)),
+    ingested_at          DateTime64(3) CODEC(DoubleDelta, ZSTD(1)),
+    score                decimal(1,1),
     status               Enum8('Cancelled' = 0, 'Completed' = 1, 'Enrolled' = 2, 'In progress' = 3)
 )
 ENGINE = ReplicatedMergeTree('/clickhouse/tables/{shard}/{database}/{table}', '{replica}')
-ORDER BY (status, enrolled_at)
+ORDER BY (status, ingested_at)
 SETTINGS index_granularity = 8192;
 
 
